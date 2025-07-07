053a50ec6c0b8a9c7ff3d1cc76473e51e93f7092
Jeremy Frank <jeremy@autonomys.xyz>
2025-07-07 10:48:58 -0600

    chore: remove unused "packages" folder

--- packages/ui/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// UI Components exports
-// Add component exports here as they are created
-
-// Example:
-// export { Button } from './Button';
-// export { Card } from './Card';
-
-// Placeholder until components are implemented
-export const componentsPlaceholder = 'UI components will be exported from here';
--- packages/ui/src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-// UI Utilities exports
-// Add utility function exports here as they are created
-
-// Example:
-// export { cn } from './cn';
-// export { formatDate } from './date';
-
-// Placeholder until utilities are implemented
-export const placeholder = 'UI utilities will be exported from here';
