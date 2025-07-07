d606c0dfee1867947d637aa686227430f12141bc
Jeremy Frank <jeremy@autonomys.xyz>
2025-07-07 10:49:46 -0600

    chore: remove unused "indexer" folder

--- apps/indexer/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Auto-Portal Indexer
-// Backend service for indexing Autonomys Network data
-
-console.log('Auto-Portal Indexer starting...');
-
-// TODO: Implement indexer functionality
-export {};
