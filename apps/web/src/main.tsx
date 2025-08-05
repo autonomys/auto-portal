7d7f930b5ab021750ce15df1f8a26166a95085b5
Jeremy Frank <jeremy@autonomys.xyz>
2025-08-05 14:48:24 -0600

    feat: implement feature flags for indexer integration and refactor App component to conditionally use ApolloProvider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import { router } from './router';
-import indexerService from './services/indexer-service';
+import { App } from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ApolloProvider client={indexerService.getClient()}>
-      <RouterProvider router={router} />
-    </ApolloProvider>
+    <App />
   </StrictMode>,
 );
