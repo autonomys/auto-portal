61eb2c0b39e6a10c0a7c9228722c5a5e55d64e19
Jeremy Frank <jeremy@autonomys.xyz>
2025-09-02 13:59:17 -0600

    feat: update TARGET_OPERATORS to include additional operators for mainnet

--- apps/web/src/constants/target-operators.ts
+++ apps/web/src/constants/target-operators.ts
@@ -1,3 +1,4 @@
 // Target operators for mainnet
-// These are the 2 available operators on mainnet
-export const TARGET_OPERATORS = ['0', '1'];
+// These are the 4 currently available operators on mainnet
+// TODO: Remove this once we have indexer in place
+export const TARGET_OPERATORS = ['0', '1', '2', '3'];
